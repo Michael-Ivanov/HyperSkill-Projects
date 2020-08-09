8e4c736e2a3e236609cf3010e866e371cf3e4267
Michael Ivanov <rattys@yandex.ru>
2020-08-09 18:17:22 +0300

    Encryption-Decryption project stage 4 completed. Program gets input parameters from command line

--- Encryption-Decryption/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/src/encryptdecrypt/Main.java
@@ -1,25 +1,36 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String direction = scanner.nextLine();
-        String line = scanner.nextLine();
-        int shift = scanner.nextInt();
-        StringBuilder resultLine = new StringBuilder();
-        if ("enc".equals(direction)) {
-            for (int i = 0; i < line.length(); i++) {
-                resultLine.append((char) (line.charAt(i) + shift));
+        String mode = "enc";
+        int key = 0;
+        String data = "";
+        for (int i = 0; i < args.length; i++) {
+            if ("-mode".equals(args[i])) {
+                mode = args[i + 1];
             }
-        } else if ("dec".equals(direction)) {
-            for (int i = 0; i < line.length(); i++) {
-                resultLine.append((char) (line.charAt(i) - shift));
+            if ("-key".equals(args[i])) {
+                key = Integer.parseInt(args[i + 1]);
+            }
+            if ("-data".equals(args[i])) {
+                data = args[i + 1];
+            }
+        }
+        StringBuilder resultLine = new StringBuilder();
+        if ("enc".equals(mode)) {
+            for (int i = 0; i < data.length(); i++) {
+                resultLine.append((char) (data.charAt(i) + key));
+            }
+        } else if ("dec".equals(mode)) {
+            for (int i = 0; i < data.length(); i++) {
+                resultLine.append((char) (data.charAt(i) - key));
             }
         } else {
             throw new IllegalArgumentException("Incorrect target operation. Should be \"enc\" or \"dec\".");
         }
         System.out.println(resultLine.toString());
+
+
+
     }
 }
