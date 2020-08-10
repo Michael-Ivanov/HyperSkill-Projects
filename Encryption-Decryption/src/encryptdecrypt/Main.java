8c0aa49182101174ef6cfe67206ad8dc18c60af9
Michael Ivanov <rattys@yandex.ru>
2020-08-10 20:32:40 +0300

    Encryption-Decryption project Stage 5 completed. Program can read from file and write to file.

--- Encryption-Decryption/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/src/encryptdecrypt/Main.java
@@ -1,21 +1,64 @@
 package encryptdecrypt;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
         String mode = "enc";
         int key = 0;
         String data = "";
+        String output = "";
+        String input = "";
+        boolean inputDefined = false;
         for (int i = 0; i < args.length; i++) {
-            if ("-mode".equals(args[i])) {
-                mode = args[i + 1];
-            }
-            if ("-key".equals(args[i])) {
-                key = Integer.parseInt(args[i + 1]);
-            }
-            if ("-data".equals(args[i])) {
-                data = args[i + 1];
+            switch (args[i]) {
+                case "-mode":
+                    mode = args[i + 1];
+                    break;
+                case "-key":
+                    key = Integer.parseInt(args[i + 1]);
+                    break;
+                case "-data":
+                    if (inputDefined) {
+                        throw new IllegalArgumentException("Too many input parameters");
+                    } else {
+                        data = args[i + 1];
+                        inputDefined = true;
+                    }
+                    break;
+                case "-in":
+                    if (inputDefined) {
+                        throw new IllegalArgumentException("Too many input parameters");
+                    } else {
+                        input = args[i + 1];
+                        File file = new File(input);
+                        try (Scanner scanner = new Scanner(file)) {
+                            data = scanner.nextLine();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case "-out":
+                    output = args[i + 1];
+                    break;
             }
         }
+        if (output.equals("")) {
+            System.out.println(lineModification(mode, data, key));
+        } else {
+            try (FileWriter fileWriter = new FileWriter(output)) {
+                fileWriter.write(lineModification(mode, data, key));
+            } catch (IOException e) {
+                System.out.println("File not found: " + output);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String lineModification(String mode, String data, int key) {
         StringBuilder resultLine = new StringBuilder();
         if ("enc".equals(mode)) {
             for (int i = 0; i < data.length(); i++) {
@@ -28,9 +71,6 @@ public class Main {
         } else {
             throw new IllegalArgumentException("Incorrect target operation. Should be \"enc\" or \"dec\".");
         }
-        System.out.println(resultLine.toString());
-
-
-
+        return resultLine.toString();
     }
 }
